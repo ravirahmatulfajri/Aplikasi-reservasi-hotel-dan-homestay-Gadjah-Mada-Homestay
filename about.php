47a196dd96dd007fdc666d4599b05342e9d87073
Ilham Nuruddin Al Huda <inurhuda00@gmail.com>
2021-10-06 10:58:39 +0700

    memperbaiki submit cek kamar
    Signed-off-by: Ilham Nuruddin Al Huda <inurhuda00@gmail.com>

--- about.php
+++ about.php
@@ -1,4 +1,3 @@
-
 <?php
 include "incl/connect.inc.php";
 include "templates/header.php";
@@ -7,179 +6,181 @@ $kueri = "select * from tb_manag_site where menu = 'Home'";
 $qr  = mysqli_query($connect, $kueri);
 
 ?>
-   <body>     
-   
-   
-		 <!-- slider Area Start-->
-		 <div class="slider-area">
-            <div class="single-slider hero-overly slider-height2 d-flex align-items-center" data-background="assets/img/hero/servicespage_hero.jpg" >
+
+<body>
+
+    <!-- slider Area Start-->
+    <div class="slider-area">
+        <div class="single-slider hero-overly slider-height2 d-flex align-items-center" data-background="assets/img/hero/servicespage_hero.jpg">
+            <div class="container">
+                <div class="row ">
+                    <div class="col-md-11 offset-xl-1 offset-lg-1 offset-md-1">
+                        <div class="hero-caption">
+                            <span>About</span>
+                            <h2>Gadjah Mada Homestay</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- slider Area End-->
+
+    <!-- Start Align Area -->
+    <div class="whole-wrap">
+        <div class="container box_1170">
+            <div class="section-top-border">
+                <center>
+                    <h1 class="mb-30">Gadjah Mada Homestay</h1>
+                </center>
+                <div class="row">
+                    <!-- <div class="col-md-12"> -->
+                    <div class="col-md-3">
+                        <img src="assets/img/about/h1_hero.jpg" alt="" class="img-fluid">
+                    </div>
+                    <div class="col-md-9 mt-sm-20">
+                        <?php
+                        while ($data = mysqli_fetch_array($qr)) :
+                            $isi_menu = $data['isi_menu'];
+                        ?>
+
+                            <?= $isi_menu ?>
+
+                        <?php endwhile; ?> </p>
+                    </div>
+                </div>
+            </div>
+            <!-- Make customer End-->
+
+            <div class="section-top-border">
+                <h2>Image Gallery</h2>
+                <div class="row gallery-item">
+                    <div class="col-md-4">
+                        <a href="assets/img/about/g1.jpg" class="img-pop-up">
+                            <div class="single-gallery-image" style="background: url(assets/img/about/g1.jpg);"></div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="assets/img/about/g2.jpg" class="img-pop-up">
+                            <div class="single-gallery-image" style="background: url(assets/img/about/g2.jpg);"></div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="assets/img/about/g3.jpg" class="img-pop-up">
+                            <div class="single-gallery-image" style="background: url(assets/img/about/g3.jpg);"></div>
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="assets/img/about/g4.jpg" class="img-pop-up">
+                            <div class="single-gallery-image" style="background: url(assets/img/about/g4.jpg);"></div>
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="assets/img/about/g5.jpg" class="img-pop-up">
+                            <div class="single-gallery-image" style="background: url(assets/img/about/g5.jpg);"></div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="assets/img/about/g6.jpg" class="img-pop-up">
+                            <div class="single-gallery-image" style="background: url(assets/img/about/g6.jpg);"></div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="assets/img/about/g7.jpg" class="img-pop-up">
+                            <div class="single-gallery-image" style="background: url(assets/img/about/g7.jpg);"></div>
+                        </a>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="assets/img/about/g8.jpg" class="img-pop-up">
+                            <div class="single-gallery-image" style="background: url(assets/img/about/g8.jpg);"></div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- Testimonial Start -->
+            <div class="testimonial-area t-padding">
+                <h2>Our Rooms</h2>
                 <div class="container">
-                    <div class="row ">
-                        <div class="col-md-11 offset-xl-1 offset-lg-1 offset-md-1">
-                            <div class="hero-caption">
-                                <span>About</span>
-                                <h2>Gadjah Mada Homestay</h2>
+                    <div class="row justify-content-center">
+                        <div class="col-xl-9 col-lg-9 col-md-9">
+                            <div class="h1-testimonial-active">
+                                <!-- Single Testimonial -->
+                                <div class="single-testimonial pt-65">
+                                    <!-- Testimonial tittle -->
+                                    <div class="font-back-tittle mb-105">
+                                        <div class="archivment-front">
+                                            <!-- <img src="assets/img/rooms/Wisan.jpg" alt=""> -->
+                                        </div>
+
+                                    </div>
+                                    <!-- Testimonial Content -->
+                                    <div class="testimonial-caption text-center">
+                                        <p>Wisma<br>Room Rate : 195000 | Fasilitas Ruangan : AC, TV, Kulkas, DVD.<br>
+                                        <div class="testimonial-ratting">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </div>
+                                        </p>
+                                    </div>
+                                </div>
+                                <!-- Single Testimonial -->
+                                <div class="single-testimonial  pt-65">
+                                    <!-- Testimonial tittle -->
+                                    <div class="font-back-tittle mb-105">
+                                        <div class="archivment-front">
+                                            <!-- <img src="assets/img/rooms/VIPRoomB.jpg" alt=""> -->
+                                        </div>
+
+                                    </div>
+                                    <!-- Testimonial Content -->
+                                    <div class="testimonial-caption text-center">
+                                        <p>Home<br>Room Rate : 205000 | Fasilitas Ruangan : Sofa, Spring Bed, TV, Kulkas, DVD Player.<br>
+                                        <div class="testimonial-ratting">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                        </div>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="section-top-border">
+                        <h3 class="mb-30">Policies</h3>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="single-defination">
+                                    <h4 class="mb-20">Homestay C4</h4>
+                                    <p>• Check-in time: After 15:00 on the day of arrival.<br>• Check-out time: 11:00 upon day of departure.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="single-defination">
+                                    <h4 class="mb-20">Wisma Kaliurang</h4>
+                                    <p>• Check-in time: After 15:00 on the day of arrival.<br> • Check-out time: 11:00 upon day of departure.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="single-defination">
+                                    <h4 class="mb-20">Homestay</h4>
+                                    <p>• Smoking: Smoking is allowed.<br> • Pets: No pets allowed</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- slider Area End-->
-
-        <!-- Start Align Area -->
-	<div class="whole-wrap">
-		<div class="container box_1170">
-			<div class="section-top-border">
-				<center><h1 class="mb-30">Gadjah Mada Homestay</h1></center>
-				<div class="row">
-					<!-- <div class="col-md-12"> -->
-                    <div class="col-md-3">
-						<img src="assets/img/about/h1_hero.jpg" alt="" class="img-fluid">
-					</div>
-					<div class="col-md-9 mt-sm-20">
-                    <?php
-                                  while ($data = mysqli_fetch_array($qr)) :
-                                      $isi_menu = $data['isi_menu'];
-                                  ?>
-
-                                      <?= $isi_menu ?>
-
-                                  <?php endwhile; ?> </p>
-					</div>
-				</div>
-			</div>
-        <!-- Make customer End-->
-        
-            <div class="section-top-border">
-				<h2>Image Gallery</h2>
-				<div class="row gallery-item">
-					<div class="col-md-4">
-						<a href="assets/img/about/g1.jpg" class="img-pop-up">
-							<div class="single-gallery-image" style="background: url(assets/img/about/g1.jpg);"></div>
-						</a>
-					</div>
-					<div class="col-md-4">
-						<a href="assets/img/about/g2.jpg" class="img-pop-up">
-							<div class="single-gallery-image" style="background: url(assets/img/about/g2.jpg);"></div>
-						</a>
-					</div>
-					<div class="col-md-4">
-						<a href="assets/img/about/g3.jpg" class="img-pop-up">
-							<div class="single-gallery-image" style="background: url(assets/img/about/g3.jpg);"></div>
-						</a>
-					</div>
-					<div class="col-md-6">
-						<a href="assets/img/about/g4.jpg" class="img-pop-up">
-							<div class="single-gallery-image" style="background: url(assets/img/about/g4.jpg);"></div>
-						</a>
-					</div>
-					<div class="col-md-6">
-						<a href="assets/img/about/g5.jpg" class="img-pop-up">
-							<div class="single-gallery-image" style="background: url(assets/img/about/g5.jpg);"></div>
-						</a>
-                    </div>
-                   <div class="col-md-4">
-						<a href="assets/img/about/g6.jpg" class="img-pop-up">
-							<div class="single-gallery-image" style="background: url(assets/img/about/g6.jpg);"></div>
-						</a>
-					</div>
-					<div class="col-md-4">
-						<a href="assets/img/about/g7.jpg" class="img-pop-up">
-							<div class="single-gallery-image" style="background: url(assets/img/about/g7.jpg);"></div>
-						</a>
-					</div>
-					<div class="col-md-4">
-						<a href="assets/img/about/g8.jpg" class="img-pop-up">
-							<div class="single-gallery-image" style="background: url(assets/img/about/g8.jpg);"></div>
-						</a>
-                    </div>
-				</div>
-			</div>
- <!-- Testimonial Start -->
-<div class="testimonial-area t-padding">
- <h2>Our Rooms</h2>
-            <div class="container">
-               <div class="row justify-content-center">
-                    <div class="col-xl-9 col-lg-9 col-md-9">
-                        <div class="h1-testimonial-active">
-                            <!-- Single Testimonial -->
-                            <div class="single-testimonial pt-65">
-                                <!-- Testimonial tittle -->
-                                <div class="font-back-tittle mb-105">
-                                    <div class="archivment-front">
-                                        <!-- <img src="assets/img/rooms/Wisan.jpg" alt=""> -->
-                                    </div>
-                                    
-                                </div>
-                                 <!-- Testimonial Content -->
-                                <div class="testimonial-caption text-center">
-                                    <p>Wisma<br>Room Rate : 195000 | Fasilitas Ruangan : AC, TV, Kulkas, DVD.<br>
-                                    <div class="testimonial-ratting">
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                    </div>
-                                    </p>
-                                </div>
-                            </div>
-                            <!-- Single Testimonial -->
-                            <div class="single-testimonial  pt-65">
-                                <!-- Testimonial tittle -->
-                                <div class="font-back-tittle mb-105">
-                                    <div class="archivment-front">
-                                        <!-- <img src="assets/img/rooms/VIPRoomB.jpg" alt=""> -->
-                                    </div>
-                                   
-                                </div>
-                                <!-- Testimonial Content -->
-                                <div class="testimonial-caption text-center">
-                                    <p>Home<br>Room Rate : 205000 | Fasilitas Ruangan : Sofa, Spring Bed, TV, Kulkas, DVD Player.<br>
-                                    <div class="testimonial-ratting">
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                    </div>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-               </div>
-               <div class="section-top-border">
-				<h3 class="mb-30">Policies</h3>
-				<div class="row">
-					<div class="col-md-4">
-						<div class="single-defination">
-							<h4 class="mb-20">Homestay C4</h4>
-							<p>• Check-in time: After 15:00 on the day of arrival.<br>• Check-out time: 11:00 upon day of departure.
-</p>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="single-defination">
-							<h4 class="mb-20">Wisma Kaliurang</h4>
-							<p>• Check-in time: After 15:00 on the day of arrival.<br> • Check-out time: 11:00 upon day of departure.
-</p>
-						</div>
-					</div>
-					<div class="col-md-4">
-						<div class="single-defination">
-							<h4 class="mb-20">Homestay</h4>
-							<p>• Smoking: Smoking is allowed.<br> • Pets: No pets allowed</p>
-						</div>
-					</div>
-				</div>
-			</div>
-            </div>
-        </div>
-        </div>
-	</div>
-        <!-- Testimonial End -->
-<?php
-include "templates/footer.php";
-?>
+    </div>
+    <!-- Testimonial End -->
+    <?php
+    include "templates/footer.php";
+    ?>
--- reservations.php
+++ reservations.php
@@ -1,6 +1,7 @@
 <?php
 include "./incl/connect.inc.php";
 include_once("./incl/function.php");
+include './incl/config.php';
 
 $kueri = "select * from tb_kamar_tipe where id_tipe = $_GET[id]";
 $qr  = mysqli_query($connect, $kueri);
@@ -13,7 +14,7 @@ while ($data = mysqli_fetch_array($qr)) {
 
 <div class="text-center pt-40">
   <h2>
-    Mardliyyah Islamic Centre UGM
+    <?= $web_title ?>
   </h2>
   <span>
     YOGYAKARTA - INDONESIA
@@ -21,7 +22,7 @@ while ($data = mysqli_fetch_array($qr)) {
 </div>
 
 <!-- Booking Room Start-->
-<div class="booking-area" style="margin-top: 40px !important;">
+<div class="booking-area" style="margin: 6rem 0 !important;">
   <div class="container">
     <div class="row ">
       <div class="col-12">
@@ -37,7 +38,7 @@ while ($data = mysqli_fetch_array($qr)) {
               </div>
               <div class="boking-datepicker">
 
-                <input id="datepicker1" name="tgl_cekin" autocomplete="off" placeholder="<?= date('d/m/Y', strtotime($Date . ' + 1 days')); ?>" data-date-format="d/m/Y" />
+                <input id="datepicker1" data-provide="datepicker" data-date-format="m/d/Y" name="tgl_cekin" autocomplete="off" value="<?= date('m/d/Y', strtotime($Date . ' + 1 days')); ?>" />
               </div>
             </div>
 
@@ -51,7 +52,7 @@ while ($data = mysqli_fetch_array($qr)) {
                 <span>Check OutDate:</span>
               </div>
               <div class="boking-datepicker">
-                <input id="datepicker2" name="tgl_cekout" autocomplete="off" placeholder="<?= date('d/m/Y', strtotime($Date . ' + 2 days')); ?>" data-date-format="d/m/Y" />
+                <input id="datepicker2" data-provide="datepicker" data-date-format="m/d/Y" name="tgl_cekout" autocomplete="off" value="<?= date('m/d/Y', strtotime($Date . ' + 2 days')); ?>" />
               </div>
             </div>
 
@@ -67,143 +68,6 @@ while ($data = mysqli_fetch_array($qr)) {
             <!-- Single Select Box -->
             <div class="single-select-box pt-45 mb-30">
               <input name="checking_room" type="submit" class="btn select-btn" id="kirim" value="Cek Kamar">
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Booking Room End-->
-
-<div class="booking-area" style="margin-top: 40px !important;">
-  <div class="container">
-    <div class="row ">
-      <div class="col-12">
-
-        <h4 class="pb-10"><strong>Cek Status Kamar </strong></h4>
-
-        <form onSubmit="return checkrequired(this)" id="form1" name="form1" method="post" action="functions.php">
-          <div class="booking-wrap d-flex flex-md-column justify-content-between align-items-center align-items-md-start">
-
-            <div class="d-flex flex-column flex-md-row row container-fluid pt-45 mb-30">
-
-              <div class="col-12 col-md-6 mb-30">
-
-                <div class="boking-tittle">
-                  <span>Tgl Chek In</span>
-                </div>
-
-                <style>
-                  .nice-select.mx-2 {
-                    padding: 0 2rem 0 1rem;
-                  }
-                </style>
-
-                <div class="d-flex">
-                  <select name="required_tgl_cekin" size="1" class="mx-2">
-                    <?php
-                    for ($i = 1; $i <= 31; $i++) {
-                      if ($i == $indate) {
-                        $selectdate = "selected";
-                      } else {
-                        $selectdate = "";
-                      }
-                      echo "<option value=\"$i\" $selectdate>$i</option>" . "\n";
-                    }
-                    ?>
-                  </select>
-
-                  <select name="required_bln_cekin" size="1" id="bln" class="mx-2">
-                    <?php
-                    for ($i = 1; $i <= 12; $i++) {
-                      if ($i == $inmonth) {
-                        $selectmonth = "selected";
-                      } else {
-                        $selectmonth = "";
-                      }
-                      echo "<option value=\"$i\" $selectmonth>$bulan[$i]</option>" . "\n";
-                    }
-                    ?>
-                  </select>
-                  <select name="required_thn_cekin" size="1" id="thn" class="mx-2">
-                    <?php
-                    for ($i = $inyear; $i <= $inyear + 1; $i++) {
-                      if ($i == $inyear) {
-                        $selectyear = "selected";
-                      } else {
-                        $selectyear = "";
-                      }
-                      echo "<option value=\"$i\" $selectyear>$i</option>" . "\n";
-                    }
-                    ?>
-                  </select>
-                </div>
-
-              </div>
-
-              <div class="col-12 col-md-6 mb-30">
-
-                <div class="boking-tittle">
-                  <span>Tgl Cek Out</span>
-                </div>
-
-                <div class="d-flex">
-                  <select name="required_tgl_cekout" size="1" id="tgl_cekout" class="mx-2">
-                    <?php
-                    for ($i = 1; $i <= 31; $i++) {
-                      if ($i == $outdate) {
-                        $selectdate = "selected";
-                      } else {
-                        $selectdate = "";
-                      }
-                      echo "<option value=\"$i\" $selectdate>$i</option>" . "\n";
-                    }
-                    ?>
-                  </select>
-                  <select name="required_bln_cekout" size="1" id="select" class="mx-2">
-                    <?php
-                    for ($i = 1; $i <= 12; $i++) {
-                      if ($i == $outmonth) {
-                        $selectmonth = "selected";
-                      } else {
-                        $selectmonth = "";
-                      }
-                      echo "<option value=\"$i\" $selectmonth>$bulan[$i]</option>" . "\n";
-                    }
-                    ?>
-                  </select>
-                  <select name="required_thn_cekout" size="1" id="select2" class="mx-2">
-                    <?php
-                    for ($i = $outyear; $i <= $outyear + 1; $i++) {
-                      if ($i == $outyear) {
-                        $selectyear = "selected";
-                      } else {
-                        $selectyear = "";
-                      }
-                      echo "<option value=\"$i\" $selectyear>$i</option>" . "\n";
-                    }
-                    ?>
-                  </select>
-                </div>
-              </div>
-            </div>
-
-            <div class="row container-fluid pt-45 mb-30">
-              <div class="single-select-box col-md-6 mb-30">
-                <div class="boking-tittle">
-                  <span>Tipe</span>
-                </div>
-                <div class="select-this">
-                  <div>
-                    <h5><?= $tipe_kamar ?></h5>
-                    <input name="id_tipe" type="hidden" value="<?php echo "$id_tipe" ?>">
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <input name="checking_room" type="submit" class="btn select-btn" id="kirim" value="Cek Kamar">
-              </div>
             </div>
           </div>
         </form>
